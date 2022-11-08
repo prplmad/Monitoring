5243ef758d7679dc6fab051610f7d05b674eaf4d
prplmad <prplmad@gmail.com>
2022-11-08 14:36:41 +0300

    Правки по PR

--- Persistence/Migrations/AddStatisticTable_20221107.cs
+++ Persistence/Migrations/AddStatisticTable_20221107.cs
@@ -20,9 +20,9 @@ public class AddStatisticTable_20221107 : Migration
         Create.Table("statistic")
             .WithColumn("id").AsInt64().PrimaryKey().Identity()
             .WithColumn("external_id").AsInt64().NotNullable()
-            .WithColumn("username").AsString()
-            .WithColumn("client_version").AsString()
-            .WithColumn("os").AsString()
-            .WithColumn("update_date").AsDateTime();
+            .WithColumn("username").AsString().NotNullable()
+            .WithColumn("client_version").AsString().NotNullable()
+            .WithColumn("os").AsString().NotNullable()
+            .WithColumn("update_date").AsDateTime().NotNullable();
     }
 }
---
+++ Persistence/Migrations/EditStatisticTable_20221108.cs
@@ -0,0 +1,30 @@
+﻿using FluentMigrator;
+
+namespace Persistence.Migrations;
+/// <summary>
+/// Миграция 20221108.
+/// </summary>
+[Migration(20221108)]
+public class EditStatisticTable_20221108 : Migration
+{
+    /// <inheritdoc/>
+    public override void Down()
+    {
+        Alter.Table("statistic")
+            .AlterColumn("username").AsString().NotNullable()
+            .AlterColumn("client_version").AsString().NotNullable()
+            .AlterColumn("os").AsString().NotNullable()
+            .AlterColumn("update_date").AsDateTime().NotNullable();
+    }
+
+    /// <inheritdoc/>
+    public override void Up()
+    {
+        Alter.Table("statistic")
+            .AlterColumn("username").AsString().Nullable()
+            .AlterColumn("client_version").AsString().Nullable()
+            .AlterColumn("os").AsString().Nullable()
+            .AlterColumn("update_date").AsDateTime().Nullable();
+    }
+
+}
